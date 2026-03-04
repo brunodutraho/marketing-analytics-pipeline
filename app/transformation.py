b40638d3e938cf5104aacae4ec0f09dd6098e7f2
Bruno Dutra <brunodutra.ho@gmail.com>
2026-03-04 12:59:01 -0300

    fix: handle division by zero in KPI calculations to prevent infinete numeric values

--- app/transformation.py
+++ app/transformation.py
@@ -1,7 +1,16 @@
+import numpy as np
 def transform_data(df):
-    df["ctr"] = df["clicks"] / df["impressions"]
-    df["cpc"] = df["cost"] / df["clicks"]
-    df["cpa"] = df["cost"] / df["conversions"]
-    df["roas"] = df["revenue"] / df["cost"]
+
+    df["ctr"] = np.where(df["impressions"] > 0,
+                        df["clicks"] / df["impressions"], 0)
+
+    df["cpc"] = np.where(df["clicks"] > 0,
+                        df["cost"] / df["clicks"], 0)
+
+    df["cpa"] = np.where(df["conversions"] > 0,
+                        df["cost"] / df["conversions"], 0)
+
+    df["roas"] = np.where(df["cost"] > 0,
+                        df["revenue"] / df["cost"], 0)
 
     return df
